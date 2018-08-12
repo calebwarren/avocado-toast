ab4eb42f1ae8e3709ee2a9e867c1275e9626d8a9
Caleb <Caleb@Janelles-MacBook-Pro.local>
2018-08-12 07:49:34 -0700

    aug12_7-49am

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './Header.css'
 
 const Header = ({ siteTitle }) => (
   <div className="Header">
     <div className="HeaderGroup">
-      <Link to="/"><img src="" width="30" /></Link>
+      <Link to="/"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Emoji_u1f951.svg/2000px-Emoji_u1f951.svg.png" width="30" /></Link>
       <Link to="/courses">Courses</Link>
       <Link to="/downloads">Downloads</Link>
       <Link to="/workshops">Workshops</Link>
-      <Link to="/buy"><button>buy</button></Link>
+      <Link to="/buy"><button>Buy</button></Link>
     </div>
   </div>
 )
